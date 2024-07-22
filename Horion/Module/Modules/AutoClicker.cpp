bc3f4022c7a5904485012561f822635d18290f01
TTOcelot <tmasdelox@gmail.com>
2024-07-22 04:07:17 -0600

    Update AutoClicker.cpp
    is this can work for increase the amount of cps?

--- Horion/Module/Modules/AutoClicker.cpp
+++ Horion/Module/Modules/AutoClicker.cpp
@@ -1,57 +1,60 @@
 #include "AutoClicker.h"
 
 AutoClicker::AutoClicker() : IModule(0, Category::COMBAT, "A simple autoclicker, automatically clicks for you.") {
-	this->registerBoolSetting("rightclick", &this->rightclick, rightclick);
-	this->registerBoolSetting("only weapons", &this->weapons, this->weapons);
-	this->registerBoolSetting("break blocks", &this->breakBlocks, this->breakBlocks);
-	this->registerIntSetting("delay", &this->delay, this->delay, 0, 20);
-	this->registerBoolSetting("hold", &this->hold, this->hold);
+    this->registerBoolSetting("rightclick", &this->rightclick, rightclick);
+    this->registerBoolSetting("only weapons", &this->weapons, this->weapons);
+    this->registerBoolSetting("break blocks", &this->breakBlocks, this->breakBlocks);
+    this->registerIntSetting("delay", &this->delay, this->delay, 1, 30); // Ajustamos el rango de delay para permitir hasta 30 cps
+    this->registerBoolSetting("hold", &this->hold, this->hold);
 }
 
 AutoClicker::~AutoClicker() {
 }
 
 const char* AutoClicker::getModuleName() {
-	return ("AutoClicker");
+    return ("AutoClicker");
 }
 
 void AutoClicker::onTick(C_GameMode* gm) {
-	if ((GameData::isLeftClickDown() || !hold) && GameData::canUseMoveKeys()) {
-		C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
-		PointingStruct* pointing = g_Data.getClientInstance()->getPointerStruct();
-		Odelay++;
+    static float clickTimer = 0; // Acumulador fraccional
 
-		if (Odelay >= delay) {
-			auto selectedItem = localPlayer->getSelectedItem();
-			if (weapons && selectedItem->getAttackingDamageWithEnchants() < 1)
-				return;
+    if ((GameData::isLeftClickDown() || !hold) && GameData::canUseMoveKeys()) {
+        C_LocalPlayer* localPlayer = g_Data.getLocalPlayer();
+        PointingStruct* pointing = g_Data.getClientInstance()->getPointerStruct();
+        clickTimer += (1.0f / 20.0f); // Incrementamos el temporizador fraccional en cada tick
 
-			g_Data.leftclickCount++;
+        if (clickTimer >= (1.0f / delay)) { // Comparamos con el tiempo calculado
+            auto selectedItem = localPlayer->getSelectedItem();
+            if (weapons && selectedItem->getAttackingDamageWithEnchants() < 1)
+                return;
 
-			localPlayer->swingArm();
+            g_Data.leftclickCount++;
 
-			if (pointing->hasEntity() != 0)
-				gm->attack(pointing->getEntity());
-			else if (breakBlocks) {
-				bool isDestroyed = false;
-				gm->startDestroyBlock(pointing->block, pointing->blockSide, isDestroyed);
-				gm->stopDestroyBlock(pointing->block);
-				if (isDestroyed && localPlayer->region->getBlock(pointing->block)->blockLegacy->blockId != 0)
-					gm->destroyBlock(&pointing->block, 0);
-			}
-			Odelay = 0;
-		}
-	}
+            localPlayer->swingArm();
 
-	if (rightclick) {
-		if ((GameData::isRightClickDown() || !hold) && GameData::canUseMoveKeys()) {
-			PointingStruct* pstruct = g_Data.getClientInstance()->getPointerStruct();
-			Odelay++;
-			if (Odelay >= delay) {
-				g_Data.rightclickCount++;
-				gm->buildBlock(new vec3_ti(pstruct->block), pstruct->blockSide);
-				Odelay = 0;
-			}
-		}
-	}
+            if (pointing->hasEntity() != 0)
+                gm->attack(pointing->getEntity());
+            else if (breakBlocks) {
+                bool isDestroyed = false;
+                gm->startDestroyBlock(pointing->block, pointing->blockSide, isDestroyed);
+                gm->stopDestroyBlock(pointing->block);
+                if (isDestroyed && localPlayer->region->getBlock(pointing->block)->blockLegacy->blockId != 0)
+                    gm->destroyBlock(&pointing->block, 0);
+            }
+            clickTimer = 0; // Reseteamos el temporizador fraccional
+        }
+    }
+
+    if (rightclick) {
+        if ((GameData::isRightClickDown() || !hold) && GameData::canUseMoveKeys()) {
+            PointingStruct* pstruct = g_Data.getClientInstance()->getPointerStruct();
+            clickTimer += (1.0f / 20.0f); // Incrementamos el temporizador fraccional en cada tick
+
+            if (clickTimer >= (1.0f / delay)) { // Comparamos con el tiempo calculado
+                g_Data.rightclickCount++;
+                gm->buildBlock(new vec3_ti(pstruct->block), pstruct->blockSide);
+                clickTimer = 0; // Reseteamos el temporizador fraccional
+            }
+        }
+    }
 }
